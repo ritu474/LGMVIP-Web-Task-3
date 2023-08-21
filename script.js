29d4f8ae297348ce836e995818f5f3362825972e
Rituparna Debnath <111441612+ritu474@users.noreply.github.com>
2023-08-21 20:47:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let display = document.getElementById('display');
+
+function appendToDisplay(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = '';
+}
+
+function calculateResult() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = 'Error';
+  }
+}
+
+function clearOne(){
+    display.value = display.value.substring(0,display.value.length - 1);
+}
